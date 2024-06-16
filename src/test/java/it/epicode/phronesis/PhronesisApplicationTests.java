aa120da5222745c0e0666ec22f7c86955ef9c7c1
Danilon6 <danilonebulosi@gmail.com>
2024-06-16 17:32:17 +0200

    First commit

---
+++ src/test/java/it/epicode/phronesis/PhronesisApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.phronesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhronesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
